f0aec7bdf830ba40b46e8d1f6972462d1da8a739
Len Volk <levolkov@microsoft.com>
2022-02-17 19:58:57 -0500

    cleanup

--- templates/delegated-resource-management/subscription/_deployment.ps1
+++ templates/delegated-resource-management/subscription/_deployment.ps1
@@ -1,2 +1,4 @@
 New-AzSubscriptionDeployment -Name "Azure-Lighthouse" -Location "eastus2" -TemplateFile subscription.json -TemplateParameterFile subscription.parameters.json -Verbose
 # New-AzResourceGroupDeployment if only delegating access to the RGs
+
+#  Error wiht gov tenant https://pic.lvolk.com/0210213059.png
